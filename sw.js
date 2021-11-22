b717249e308de8bbb53891fc5da126ba2ce1dfef
Libia Perez <alexandra220790@gmail.com>
2021-11-21 21:26:17 -0300

    fixinf services worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 // Cuando se insale el sw, se cree la cache
+const CACHE_NAME = "V1_cache_comprando";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -25,4 +26,54 @@ self.addEventListener("fetch", function (evento) {
     return caches.match(evento.request);
   });
   evento.respondWith(respuesta);
+});
+
+// 1. Estado: Instalacion, almacenar los archivo estaticos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.log("Error al Registrarse", err);
+      })
+  );
+});
+
+// 2. Estado Activo, Buscando los archivos del almacenado
+self.addEventListener("active", (event) => {
+  const cachelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cachesNames) => {
+        cachesNames.map((cachesName) => {
+          // Removero lo que no se necesita en cache
+          if (cachelist.indexOf(cachesName) === -1) {
+            return caches.delete(cachesName);
+          }
+        });
+      })
+      .then(() => self.clients.claim())
+      .catch((err) => {
+        console.log("Error al Registrarse", err);
+      })
+  );
+});
+
+// Cuando necesite recuperar los archivos
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      if (resp) {
+        return resp;
+      }
+
+      // Recuerar el actual
+      return fetch(event.request);
+    })
+  );
 });
