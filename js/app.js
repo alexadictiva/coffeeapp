c3c4fabe2ae0b61c4a30a54a6ae614894b9fa766
Libia Perez <alexandra220790@gmail.com>
2021-11-21 21:06:04 -0300

    Creating a pwa about coffe

---
+++ js/app.js
@@ -0,0 +1,44 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
+  console.log("Service Worker registrado");
+}
+
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    products: [],
+    carrito: [],
+    mensajePrueba: null,
+    //url: "https://rickandmortyapi.com/api/character/",
+    url: "./../local-api/localApi.json",
+  },
+  methods: {
+    fetchApi: function () {
+      fetch(this.url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.products = data.results;
+        })
+
+        .catch((err) => console.error(err));
+    },
+    agregarCarrito: function (product) {
+      console.log(
+        "producto seleccionado: " + product.id + " " + product.nombre
+      );
+      this.carrito.push(product);
+      localStorage.setItem("carrito", JSON.stringify(this.carrito));
+    },
+    mostrarCarrito: function () {
+      for (let i = 0; i < this.localStorage.carrito.length; i++) {
+        JSON.parse(this.localStorage.carrito)[i].nombre;
+      }
+    },
+  },
+});
+
+app.fetchApi();
---
+++ sw.js
@@ -0,0 +1,28 @@
+// Cuando se insale el sw, se cree la cache
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./css/bootstrap.min.css",
+  "./assets/icon/coffee-48.png",
+  "./js/app.js",
+];
+
+self.addEventListener("install", function (evento) {
+  console.log("Instalando SW");
+  const cacheStatico = caches.open("statico-v1").then((cache) => {
+    cache.addAll(urlsToCache);
+  });
+  // Cuando termine el evento
+  evento.waitUntil(cacheStatico);
+});
+
+// Cuando este offline, se carga la cache y cuando no cargue de la web
+self.addEventListener("fetch", function (evento) {
+  //      console.log('Fetching', evento.request);
+  const respuesta = fetch(evento.request).catch((error) => {
+    console.log("Error en la petición");
+    return caches.match(evento.request);
+  });
+  evento.respondWith(respuesta);
+});
